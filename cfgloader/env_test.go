127632996c04b53d0abe25ced2f0ee909c5c63a0
Oleksandr Shepetko <a@shepetko.com>
2025-02-11 11:41:50 +0200

    Fix

--- cfgloader/env_test.go
+++ cfgloader/env_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/ashep/go-cfgloader"
+	"github.com/ashep/go-app/cfgloader"
 )
 
 func TestLoadFromEnv(tt *testing.T) {
