6ad13fbe4d1d9777b751d8d75bcf194855d3a99c
Oleksandr Shepetko <a@shepetko.com>
2025-11-16 00:56:08 +0200

    dbmigrator: add verions info

--- dbmigrator/migrator.go
+++ dbmigrator/migrator.go
@@ -10,40 +10,48 @@ import (
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
-func RunPostgres(url string, fs embed.FS, path string) (uint, error) {
+type MigrationResult struct {
+	PrevVersion uint
+	NewVersion  uint
+}
+
+func RunPostgres(url string, fs embed.FS, path string) (*MigrationResult, error) {
 	srcDrv, err := iofs.New(fs, path)
 	if err != nil {
-		return 0, fmt.Errorf("load migrations: %w", err)
+		return nil, fmt.Errorf("load migrations: %w", err)
 	}
 
 	dbDrv, err := (&pgxdrv.Postgres{}).Open(url)
 	if err != nil {
-		return 0, fmt.Errorf("open db: %w", err)
+		return nil, fmt.Errorf("open db: %w", err)
 	}
 
 	mig, err := migrate.NewWithInstance("iofs", srcDrv, "postgres", dbDrv)
 	if err != nil {
-		return 0, fmt.Errorf("init migrator: %w", err)
+		return nil, fmt.Errorf("init migrator: %w", err)
 	}
 
-	ver, dirty, err := mig.Version()
+	prevVer, dirty, err := mig.Version()
 	if err != nil && !errors.Is(err, migrate.ErrNilVersion) {
-		return 0, fmt.Errorf("get db version before migration: %w", err)
+		return nil, fmt.Errorf("get db version before migration: %w", err)
 	}
 
 	if dirty {
-		return ver, fmt.Errorf("current version is dirty: %d", ver)
+		return nil, fmt.Errorf("current version is dirty: %d", prevVer)
 	}
 
 	err = mig.Up()
 	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		return 0, fmt.Errorf("apply migrations: %w", err)
+		return nil, fmt.Errorf("apply migrations: %w", err)
 	}
 
-	ver, _, err = mig.Version()
+	newVer, _, err := mig.Version()
 	if err != nil && !errors.Is(err, migrate.ErrNilVersion) {
-		return 0, fmt.Errorf("get db version after migration: %w", err)
+		return nil, fmt.Errorf("get db version after migration: %w", err)
 	}
 
-	return ver, nil
+	return &MigrationResult{
+		PrevVersion: prevVer,
+		NewVersion:  newVer,
+	}, nil
 }
