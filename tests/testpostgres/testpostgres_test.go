87a23c077c2f7eba409bb49847a499b0d8b9df45
Oleksandr Shepetko <a@shepetko.com>
2026-04-06 23:08:23 +0300

    Fix test

--- tests/testpostgres/testpostgres_test.go
+++ tests/testpostgres/testpostgres_test.go
@@ -6,13 +6,14 @@ import (
 	"context"
 	"testing"
 
+	"github.com/ashep/go-app/testlogger"
 	"github.com/ashep/go-app/testpostgres"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPostgres(main *testing.T) {
 	main.Run("Ok", func(t *testing.T) {
-		tp := testpostgres.New(t)
+		tp := testpostgres.New(t, testlogger.New(t).Logger())
 		db := tp.DB()
 		_, err := db.Exec(context.Background(), "SELECT 1;")
 		require.NoError(t, err)
