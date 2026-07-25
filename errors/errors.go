554d87a4ad943005451bb914040a33c476b6471c
Oleksandr Shepetko <a@shepetko.com>
2026-07-25 21:52:45 +0300

    Add errors

---
+++ errors/errors.go
@@ -0,0 +1,43 @@
+package errors
+
+type NotFoundError struct {
+	Subj string
+}
+
+func (e NotFoundError) Error() string {
+	return e.Subj + " is not found"
+}
+
+type AlreadyExistsError struct {
+	Subj string
+}
+
+func (e AlreadyExistsError) Error() string {
+	return e.Subj + " is already exists"
+}
+
+type AccessDeniedError struct{}
+
+func (e AccessDeniedError) Error() string {
+	return "access denied"
+}
+
+type InvalidArgError struct {
+	Subj   string
+	Reason string
+}
+
+func (e InvalidArgError) Error() string {
+	s := ""
+
+	if e.Subj != "" {
+		s = "invalid " + e.Subj
+		if e.Reason != "" {
+			s += ": "
+		}
+	}
+
+	s += e.Reason
+
+	return s
+}
---
+++ errors/errors_test.go
@@ -0,0 +1,81 @@
+package errors_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	apperrors "github.com/ashep/go-app/errors"
+)
+
+//nolint:dupl // false positive
+func TestNotFoundError(t *testing.T) {
+	t.Parallel()
+
+	err := apperrors.NotFoundError{Subj: "foo"}
+
+	assert.Equal(t, "foo is not found", err.Error())
+
+	assert.True(t, errors.Is(err, apperrors.NotFoundError{Subj: "foo"}))
+	assert.True(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.NotFoundError{Subj: "foo"}))
+
+	assert.False(t, errors.Is(err, apperrors.NotFoundError{Subj: "bar"}))
+	assert.False(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.NotFoundError{Subj: "bar"}))
+
+	assert.True(t, errors.As(err, &apperrors.NotFoundError{}))
+	assert.True(t, errors.As(fmt.Errorf("wrap: %w", err), &apperrors.NotFoundError{}))
+}
+
+//nolint:dupl // false positive
+func TestAlreadyExistsError(t *testing.T) {
+	t.Parallel()
+
+	err := apperrors.AlreadyExistsError{Subj: "foo"}
+
+	assert.Equal(t, "foo is already exists", err.Error())
+
+	assert.True(t, errors.Is(err, apperrors.AlreadyExistsError{Subj: "foo"}))
+	assert.True(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.AlreadyExistsError{Subj: "foo"}))
+
+	assert.False(t, errors.Is(err, apperrors.AlreadyExistsError{Subj: "bar"}))
+	assert.False(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.AlreadyExistsError{Subj: "bar"}))
+
+	assert.True(t, errors.As(err, &apperrors.AlreadyExistsError{}))
+	assert.True(t, errors.As(fmt.Errorf("wrap: %w", err), &apperrors.AlreadyExistsError{}))
+}
+
+func TestAccessDeniedError(t *testing.T) {
+	t.Parallel()
+
+	err := apperrors.AccessDeniedError{}
+
+	assert.Equal(t, "access denied", err.Error())
+
+	assert.True(t, errors.Is(err, apperrors.AccessDeniedError{}))
+	assert.True(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.AccessDeniedError{}))
+
+	assert.True(t, errors.As(err, &apperrors.AccessDeniedError{}))
+	assert.True(t, errors.As(fmt.Errorf("wrap: %w", err), &apperrors.AccessDeniedError{}))
+}
+
+func TestInvalidArgError(t *testing.T) {
+	t.Parallel()
+
+	err := apperrors.InvalidArgError{Subj: "foo", Reason: "theReason"}
+
+	assert.Equal(t, "invalid foo: theReason", err.Error())
+
+	assert.True(t, errors.Is(err, apperrors.InvalidArgError{Subj: "foo", Reason: "theReason"}))
+	assert.True(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.InvalidArgError{Subj: "foo", Reason: "theReason"}))
+
+	assert.False(t, errors.Is(err, apperrors.InvalidArgError{Subj: "bar", Reason: "theReason"}))
+	assert.False(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.InvalidArgError{Subj: "bar", Reason: "theReason"}))
+
+	assert.False(t, errors.Is(err, apperrors.InvalidArgError{Subj: "foo", Reason: "theOtherReason"}))
+	assert.False(t, errors.Is(fmt.Errorf("wrap: %w", err), apperrors.InvalidArgError{Subj: "foo", Reason: "theOtherReason"}))
+
+	assert.True(t, errors.As(err, &apperrors.InvalidArgError{}))
+	assert.True(t, errors.As(fmt.Errorf("wrap: %w", err), &apperrors.InvalidArgError{}))
+}
