c0c48e0b2d95e812bfd570303e0e0e714f6a599d
Oleksandr Shepetko <a@shepetko.com>
2026-08-29 09:38:46 +0300

    testlogger: new functions

--- testlogger/testlogger.go
+++ testlogger/testlogger.go
@@ -1,6 +1,7 @@
 package testlogger
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -36,16 +37,95 @@ func (l *Logger) Content() string {
 	return l.w.Content()
 }
 
+func (l *Logger) Messages() []string {
+	return strings.Split(strings.TrimRight(l.Content(), "\n"), "\n")
+}
+
+// MessagesContainsAny returns all log messages that contain any of the specified substrings.
+func (l *Logger) MessagesContainsAny(s ...string) []string {
+	res := make([]string, 0)
+	for _, m := range l.Messages() {
+		for _, sub := range s {
+			if !strings.Contains(m, sub) {
+				continue
+			}
+			res = append(res, m)
+		}
+
+	}
+	return res
+}
+
+// MessagesContainsAll returns all log messages that contain all of the specified substrings.
+func (l *Logger) MessagesContainsAll(s ...string) []string {
+	res := make([]string, 0)
+	for _, m := range l.Messages() {
+		matchesAll := true
+		for _, sub := range s {
+			if !strings.Contains(m, sub) {
+				matchesAll = false
+				break
+			}
+		}
+		if matchesAll {
+			res = append(res, m)
+		}
+	}
+	return res
+}
+
+func (l *Logger) FirstMessageContainsAny(s ...string) string {
+	msgs := l.MessagesContainsAny(s...)
+	if len(msgs) > 0 {
+		return msgs[0]
+	}
+	return ""
+}
+
+func (l *Logger) FirstMessageContainsAll(s ...string) string {
+	msgs := l.MessagesContainsAll(s...)
+	if len(msgs) > 0 {
+		return msgs[0]
+	}
+	return ""
+}
+
 func (l *Logger) AssertContains(s string) {
-	assert.Contains(l.t, l.w.Content(), s, "logs do not contain expected string")
+	assert.Contains(l.t, l.w.Content(), s)
+}
+
+func (l *Logger) AssertNotContains(s string) {
+	assert.NotContains(l.t, l.w.Content(), s)
 }
 
 func (l *Logger) AssertNoErrors() {
-	assert.NotContains(l.t, l.w.Content(), `"level":"error"`, "logs contain error level")
+	l.AssertNotContains(`"level":"error"`)
+}
+
+func (l *Logger) AssertContainsError() {
+	l.AssertContains(`"level":"error"`)
+}
+
+func (l *Logger) AssertHasError(s string) {
+	m := l.FirstMessageContainsAll(`"level":"error"`, s)
+	if m == "" {
+		assert.Fail(l.t, fmt.Sprintf("%#v should contain error message %#v", l.Content(), s))
+	}
 }
 
 func (l *Logger) AssertNoWarns() {
-	assert.NotContains(l.t, l.w.Content(), `"level":"warn"`, "logs contain warn level")
+	l.AssertNotContains(`"level":"warn"`)
+}
+
+func (l *Logger) AssertContainsWarn() {
+	l.AssertContains(`"level":"warn"`)
+}
+
+func (l *Logger) AssertHasWarn(s string) {
+	m := l.FirstMessageContainsAll(`"level":"warn"`, s)
+	if m == "" {
+		assert.Fail(l.t, fmt.Sprintf("%#v should contain warn message %#v", l.Content(), s))
+	}
 }
 
 func (l *Logger) AssertNoWarnsAndErrors() {
--- testlogger/testlogger_test.go
+++ testlogger/testlogger_test.go
@@ -4,21 +4,215 @@ import (
 	"testing"
 
 	"github.com/ashep/go-app/testlogger"
-	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLogger(main *testing.T) {
 	main.Parallel()
 
-	main.Run("Log", func(t *testing.T) {
+	main.Run("Content", func(t *testing.T) {
 		t.Parallel()
 
 		tl := testlogger.New(t)
-		l := zerolog.New(tl.Logger())
+		l := tl.Logger()
 
 		l.Info().Msg("test message")
+
 		assert.Equal(t, `{"level":"info","message":"test message"}
 `, tl.Content())
+	})
+
+	main.Run("Messages", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message 1")
+		l.Warn().Msg("test message 2")
+		l.Error().Msg("test message 3")
+
+		assert.Equal(t, []string{
+			`{"level":"info","message":"test message 1"}`,
+			`{"level":"warn","message":"test message 2"}`,
+			`{"level":"error","message":"test message 3"}`,
+		}, tl.Messages())
+	})
+
+	main.Run("MessagesContainsAny", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message 1")
+		l.Warn().Msg("test message 2")
+		l.Error().Msg("test message 3")
+
+		assert.Equal(t, []string{
+			`{"level":"warn","message":"test message 2"}`,
+			`{"level":"error","message":"test message 3"}`,
+		}, tl.MessagesContainsAny("test message 2", "test message 3"))
+	})
+
+	main.Run("FirstMessageContains", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message 1")
+		l.Warn().Msg("test message 2")
+		l.Error().Msg("test message 3")
+
+		assert.Equal(t, []string{
+			`{"level":"warn","message":"test message 2"}`,
+		}, tl.MessagesContainsAny("2"))
+	})
+
+	main.Run("MessagesContainsAll", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message 1")
+		l.Warn().Msg("test message 2")
+		l.Error().Msg("test message 3")
+
+		assert.Equal(t, []string{
+			`{"level":"warn","message":"test message 2"}`,
+		}, tl.MessagesContainsAll("warn", "message 2"))
+
+		assert.Empty(t, tl.MessagesContainsAll("warn", "message 3"))
+	})
+
+	main.Run("FirstMessageContainsAny", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message 1")
+		l.Warn().Msg("test message 2")
+
+		assert.Equal(t, `{"level":"warn","message":"test message 2"}`, tl.FirstMessageContainsAny("message 2"))
+		assert.Equal(t, "", tl.FirstMessageContainsAny("nope"))
+	})
+
+	main.Run("FirstMessageContainsAll", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message 1")
+		l.Warn().Msg("test message 2")
+
+		assert.Equal(t, `{"level":"warn","message":"test message 2"}`, tl.FirstMessageContainsAll("warn", "message 2"))
+		assert.Equal(t, "", tl.FirstMessageContainsAll("warn", "message 1"))
+	})
+
+	main.Run("AssertContains", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message")
+
+		tl.AssertContains("test message")
+	})
+
+	main.Run("AssertNotContains", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message")
+
+		tl.AssertNotContains("other message")
+	})
+
+	main.Run("AssertNoErrors", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message")
+		l.Warn().Msg("test warn")
+
+		tl.AssertNoErrors()
+	})
+
+	main.Run("AssertContainsError", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Error().Msg("test error")
+
+		tl.AssertContainsError()
+	})
+
+	main.Run("AssertHasError", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("unrelated")
+		l.Error().Msg("something failed")
+
+		tl.AssertHasError("something failed")
+	})
+
+	main.Run("AssertNoWarns", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message")
+		l.Error().Msg("test error")
+
+		tl.AssertNoWarns()
+	})
+
+	main.Run("AssertContainsWarn", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Warn().Msg("test warn")
+
+		tl.AssertContainsWarn()
+	})
+
+	main.Run("AssertHasWarn", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("unrelated")
+		l.Warn().Msg("something risky")
+
+		tl.AssertHasWarn("something risky")
+	})
+
+	main.Run("AssertNoWarnsAndErrors", func(t *testing.T) {
+		t.Parallel()
+
+		tl := testlogger.New(t)
+		l := tl.Logger()
+
+		l.Info().Msg("test message")
+
+		tl.AssertNoWarnsAndErrors()
 	})
 }
